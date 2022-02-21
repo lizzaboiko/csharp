f888f3b4f0e6e086aac273f91c56b432250a5500
lizza <lizavetaboika@mail.ru>
2022-02-22 00:05:08 +0300

    dzshka 2

---
+++ MyFirstConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyFirstConsole
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int a = 10;
+            Console.WriteLine(a);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MySecondConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MySecondConsole
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            const decimal pi = 3.141592653m;
+            const decimal e = 1.414213562373095048m;
+            Console.WriteLine(pi);
+            Console.WriteLine(e);
+        }
+    }
+}
