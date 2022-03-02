b0cc69463c6d6d6e646cece078c911a9b1a58dfb
lizza <lizavetaboika@mail.ru>
2022-03-02 18:07:14 +0300

    dzshka

---
+++ dzshka3.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace dzshka3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            const decimal pi = 3.141592653m;
+            Console.WriteLine("Введите радиус основания в см:");
+            int r = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите образующую в см:");
+            int l = int.Parse(Console.ReadLine());
+            decimal s = pi * r * (r + l);
+            Console.WriteLine($"Площадь поверхности круглого конуса равна {s} см^2 ");
+
+
+        }
+    }
+}
---
+++ dzshka3.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace dzshka3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int x = 14;
+            int y = 1;
+            int z = 5;
+            x += y - x++ * z; // операции присваивания правоассоциативные, выполение справа налево
+            Console.WriteLine($"x = {x} y = {y} z = {z}");
+            z = --x - y * 5;
+            Console.WriteLine($"x = {x} y = {y} z = {z}");
+            y /= x + 5 % z;
+            Console.WriteLine($"x = {x} y = {y} z = {z}");
+            z = x++ + y * 5;
+            Console.WriteLine($"x = {x} y = {y} z = {z}");
+            x = y - x++ * z;
+            Console.WriteLine($"x = {x} y = {y} z = {z}");
+        }
+    }
+}
