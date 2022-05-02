a9ac20560e15d944620fb2ef4bb3c3be7f81398b
lizza <lizavetaboika@mail.ru>
2022-05-02 18:01:57 +0300

    dzshka 5: №1, №2

---
+++ ClassLibraryV2/ArrayWorker.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace ClassLibraryV2
+{
+    public static class ArrayWorker
+   {
+        public static int[] Invert(in int[] mass)
+        {
+            int[] invertmass = new int[mass.Length];
+            for (int a = mass.Length-1; a>=0; a--)
+            {
+                invertmass[mass.Length-1-a] = mass[a]; 
+            }
+            //invertmass[0] = mass[mass.Length - 1];
+            //invertmass[invertmass.Length - 1] = mass[0];
+            return invertmass; 
+        }
+
+        public static int[] Insert(int[] source, int index, int newValue)
+        {
+            if (index > source.Length)
+            {
+                return source;
+            }
+            int[] sourceinserted = new int[source.Length+1];
+            sourceinserted[index] = newValue;
+            for (int a = 0; a < index ; a++)
+            {
+                sourceinserted[a] = source[a];
+            }
+            for (int a = index; a<=source.Length-1; a++)
+            {
+                sourceinserted[a+1] = source[a];
+            }
+            return sourceinserted;
+        }
+    } 
+}
+
---
+++ dzshka5.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ClassLibraryV2;
+
+namespace dzshka5
+{
+    class MainClass
+    {
+        public static void Print(in int[] array)
+        {
+            for (int a=0; a < array.Length ; a++)
+            {
+                Console.Write(array[a]);
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+        public static void Main(string[] args)
+        {
+            int[] array = { 1, 32, 3, 4, 5 };
+            int[] inverted = ArrayWorker.Invert(in array);
+            Print(in array);
+            Print(in inverted);
+            int[] source = { 1, 8, 7, 5, 9 };
+            int[] sourceinserted = ArrayWorker.Insert(source, 9, 23) ;
+            Print(source);
+            Print(sourceinserted);
+        }
+    }
+}
