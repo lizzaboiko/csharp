e6b79659c18f7f24d25620bc1b506be1b590abe2
lizza <lizavetaboika@mail.ru>
2022-05-04 14:46:54 +0300

    dzshka5.3

---
+++ ConeSqu/ClassConeSq.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace ClassLibraryCone
+{
+    public class ConeSq
+    {
+        public double r;
+        public double h;
+        public ConeSq(double r, double h)
+        {
+            this.r = r;
+            this.h = h;
+        }
+        public double SquareBase()
+        {
+            double basesq = Math.PI * r * r;
+            return basesq;
+        }
+        public double SquareCone()
+        {
+            double fullsq = Math.PI * r * (r + Math.Sqrt(r*r + h*h));
+            return fullsq;
+        }
+    }
+}
---
+++ dzshka5.2/dzshka5.2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ClassLibraryCone;
+
+namespace dzshka53
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+        ConeSq cone = new ConeSq(3.22243, 9.999837);
+            Console.WriteLine(cone.SquareBase());
+            Console.WriteLine(cone.SquareCone());
+        }
+    }
+}
